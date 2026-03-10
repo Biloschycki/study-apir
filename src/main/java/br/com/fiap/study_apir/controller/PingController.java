970c9a00d2a55c5b856257302acdfb5273a556e0
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-09 22:43:50 -0300

    final para cp 1

--- src/main/java/br/com/fiap/study_apir/controller/PingController.java
+++ src/main/java/br/com/fiap/study_apir/controller/PingController.java
@@ -1,4 +1,4 @@
-package br.com.fiap.study_apir;
+package br.com.fiap.study_apir.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +12,21 @@ public class PingController {
 
     @GetMapping("ping")//só preciso colocar barra para palavras compostas
     public String ping(){
-        return "pong";
+        return "pong blz";
     }
 
      @GetMapping("rota1")
     public String rotal(){
         return "rota1";
+    }
+
+    @GetMapping("rota2")
+    public String rota2(){
+        return "rota2";
+    }
+
+    @GetMapping("rota3")
+    public String rota3(){
+        return "rota3";
     }
 }
