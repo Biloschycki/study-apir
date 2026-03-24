df9b3dbb84ea3b370e92218e5304a78510d286d0
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-23 22:43:28 -0300

    model e repository

---
+++ src/main/java/br/com/fiap/study_apir/model/Produto.java
@@ -0,0 +1,21 @@
+package br.com.fiap.study_apir.model;
+
+public class Produto {
+    private Long id;
+    private String nome;
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    
+
+
+}
