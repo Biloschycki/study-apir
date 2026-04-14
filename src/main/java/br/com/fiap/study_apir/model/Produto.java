05efb43e25412660b2805c99346e0b8163f15ece
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-13 22:34:58 -0300

    insercao banco de dados

--- src/main/java/br/com/fiap/study_apir/model/Produto.java
+++ src/main/java/br/com/fiap/study_apir/model/Produto.java
@@ -1,11 +1,13 @@
 package br.com.fiap.study_apir.model;
 
 import java.math.BigDecimal;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
-
 @Data
+@Entity
 public class Produto {
+    @Id
     private  Long id;
     private  String nome;
     private   BigDecimal valor;
@@ -13,6 +15,30 @@ public class Produto {
     public Produto(Long id, String nome, BigDecimal valor) {
         this.id = id;
         this.nome = nome;
+        this.valor = valor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
         this.valor = valor;
     }
     
